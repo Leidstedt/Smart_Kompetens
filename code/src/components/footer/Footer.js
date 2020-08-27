76d78b59da8e6bd3183426494382172ff2a3eb8f
Your Name <you@example.com>
2020-08-27 22:38:33 +0200

    changed footer

--- code/src/components/footer/Footer.js
+++ code/src/components/footer/Footer.js
@@ -4,6 +4,8 @@ import 'components/footer/footer.css'
 export const Footer = () => (
 
     <footer className= "footer-style">
-        <img src="../../assets/logo-regionostergotland.svg" alt="Logo Region Östergötland"></img>
+        <div className="logo-box">
+            <img src="../../assets/logo-regionostergotland.svg" alt="Logo Region Östergötland"></img>
+        </div>
     </footer>
   )
